7a20a1fedc1bbd17ee1236fe0b95e01b058676ae
Nathan <nathan.clemmons@sinclair.edu>
2020-10-23 20:59:44 -0400

    Initial Commit

---
+++ Nathan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Nathan
+//
+//  Created by Nathan on 10/23/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
